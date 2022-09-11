669138de2a0b7abc65bb15b7fd0da090471b07c2
Baibhav <baibhav030501@gmail.com>
2022-09-11 17:30:31 +0530

    server files

--- server/index.js
+++ server/index.js
@@ -16,6 +16,13 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/predict", predictRoutes);
 
+// Serve Frontend
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../", "client", "build", "index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on PORT: ${PORT}`);
 });
